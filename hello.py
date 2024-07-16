a9f8135c98ef413fecd002a4b265e876e20af13b
Oleg Falko <fal123@list.ru>
2024-07-16 12:42:48 +0300

    добавил гитигноре

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
+# source .venv/bin/activate
+
+
 from rich import print
 
 print("Hello, [bold green]Geeks[/bold green]!")
 print ('Hello, Oleg')
 print ('debcore - 03072024 проверка 123 ')
- 
+print ('работаю с [bold yellow]neovim')
 
